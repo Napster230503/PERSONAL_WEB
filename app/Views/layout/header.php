59a3828a6924c97426565e48f18270be960cda6b
fahmi <fahmi@MAKOTAS.LOCAL>
2026-04-10 10:00:41 +0700

    Update 10 Apr 2026 edit header

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -228,66 +228,25 @@
   </head>
   <!--end::Head-->
   <!--begin::Body-->
-  <body class="layout-fixed sidebar-expand-lg sidebar-mini bg-body-tertiary">
+  <body class="layout-fixed sidebar-expand-lg sidebar-mini bg-body-tertiaryapp-header-fixed">
     <!--begin::App Wrapper-->
     <div class="app-wrapper">
       <!--begin::Header-->
-      <nav class="app-header navbar navbar-expand bg-body">
+      <nav class="app-header navbar navbar-expand bg-body shadow-sm sticky-top">
         <div class="container-fluid">
           <ul class="navbar-nav">
             <li class="nav-item">
               <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
-                <i class="bi bi-list"></i>
+                <i class="bi bi-list fs-4"></i>
               </a>
-            </li>
-            <li class="nav-item d-none d-md-block">
-              <a href="<?= base_url('/') ?>" class="nav-link">Home</a>
-            </li>
-            <li class="nav-item d-none d-md-block">
-              <a href="<?= base_url('contact') ?>" class="nav-link">Contact</a>
             </li>
           </ul>
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                <i class="bi bi-search"></i>
-              </a>
+              <div class="nav-link d-flex align-items-center">
+                <span class="d-none d-md-inline fw-bold me-2" style="color: #555;">Fahmi Alamsyah N.</span>
+              </div>
             </li>
-            <li class="nav-item dropdown user-menu">
-              <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                <img
-                  src="<?= base_url('assets/img/user2-160x160.jpg') ?>"
-                  class="user-image rounded-circle shadow"
-                  alt="User Image"
-                />
-                <span class="d-none d-md-inline">Fahmi Alamsyah Nugroho</span>
-              </a>
-              <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
-                <li class="user-header text-bg-primary">
-                  <img
-                    src="<?= base_url('assets/img/user2-160x160.jpg') ?>"
-                    class="rounded-circle shadow"
-                    alt="User Image"
-                  />
-                  <p>
-                    Fahmi Alamsyah Nugroho
-                    <small>Web Developer, Software Engineer, Oracle ERP Specialist</small>
-                    <!-- <small>Member since Nov. 2023</small> -->
-                  </p>
-                </li>
-                <li class="user-body">
-                  <div class="row">
-                    <div class="col-4 text-center"><a href="#">Followers</a></div>
-                    <div class="col-4 text-center"><a href="#">Sales</a></div>
-                    <div class="col-4 text-center"><a href="#">Friends</a></div>
-                  </div>
-                </li>
-                <li class="user-footer">
-                  <a href="#" class="btn btn-outline-secondary">Profile</a>
-                  <a href="#" class="btn btn-outline-danger float-end">Sign out</a>
-                </li>
-                </ul>
-            </li>
-            </ul>
-          </div>
-        </nav>
+          </ul>
+        </div>
+      </nav>
